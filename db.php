3e0ef8d200dc07f0c44c0d096597ab4f2f190ac5
Emperor <emperor@empire.local>
2026-02-25 11:09:46 +0800

    Time fixed

--- db.php
+++ db.php
@@ -1,6 +1,8 @@
 <?php
 session_start(); // Must be at the top
 require_once __DIR__ . '/includes/functions.php';
+date_default_timezone_set("Asia/Manila");
+
 
 // Show errors temporarily (for debugging)
 ini_set('display_errors', 1);
@@ -9,8 +11,8 @@ error_reporting(E_ALL);
 
 // Database connection
 $host = 'localhost';
-$user = 'root';          // <-- our MySQL user
-$pass = '';    // <-- password for 'user'
+$user = 'user';          // <-- our MySQL user
+$pass = 'mypassword';    // <-- password for 'user'
 $name = 'byu_ticketing';
 
 $conn = new mysqli($host, $user, $pass, $name);
